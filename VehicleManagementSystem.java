82fd71fa4b1065a96716591a1159ef5b54c8690c
Dwynne74 <ddbalbosa@gmail.com>
2024-12-15 17:20:23 -0700

    Changed the year

--- VehicleManagementSystem.java
+++ VehicleManagementSystem.java
@@ -1,7 +1,7 @@
 public class VehicleManagementSystem {
     public static void main(String[] args) {
 
-        Vehicle newVehicle = new Vehicle("Subaru", "Crosstrek", "Automatic", true, 2015);
+        Vehicle newVehicle = new Vehicle("Subaru", "Crosstrek", "Automatic", true, 2017);
         newVehicle.displayInfo();
     }
 }
