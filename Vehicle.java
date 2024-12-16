2629c580c0ed2178bc50bfb03eb24ff17ebdf42e
Dwynne74 <ddbalbosa@gmail.com>
2024-12-15 17:08:28 -0700

    Added starting files

---
+++ Vehicle.java
@@ -0,0 +1,47 @@
+public class Vehicle {
+
+    String make;
+    String model;
+    String transmission;
+    boolean cleanTitle;
+    int year;
+
+    public Vehicle(String make, String model, String transmission,
+            boolean cleanTitle, int year) {
+        this.make = make;
+        this.model = model;
+        this.transmission = transmission;
+        this.cleanTitle = cleanTitle;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getTitle() {
+        if (cleanTitle) {
+            return "Clean";
+        } else {
+            return "Rebuilt";
+        }
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Make: " + make + "| Model " + model + "| Year: " + year
+                + "| Transmission: " + transmission + "| Title: " + getTitle());
+    }
+
+}
---
+++ VehicleManagementSystem.java
@@ -0,0 +1,7 @@
+public class VehicleManagementSystem {
+    public static void main(String[] args) {
+
+        Vehicle newVehicle = new Vehicle("Subaru", "Crosstrek", "Automatic", true, 2015);
+        newVehicle.displayInfo();
+    }
+}
